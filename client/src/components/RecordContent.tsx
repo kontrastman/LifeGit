ea3857e6be85e000e2e3ab5b4b6a7d7b79dd111f
Anastasiia Pinkovska <akpinkovska@gmail.com>
2024-07-23 19:01:19 -0400

    --Adjusted text leading in record

--- client/src/components/RecordContent.tsx
+++ client/src/components/RecordContent.tsx
@@ -13,7 +13,7 @@ const RecordContent: React.FC<RecordProps> = ({selectedItem}) => {
             {data.map((record,index) => (
                 <div key={index} className="flex flex-column w-content pb-[15px]">
                     <TimeBadge time={record.time}/>
-                    <p className="pl-2.5">{record.text}</p>
+                    <p className="pl-2.5 leading-1.5">{record.text}</p>
                 </div>
             ))
             }
